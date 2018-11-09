6dee1b24e7d4fbf6024c196da36262a8ea56b6fb
Bruno Cunha <bruno@brunocunha.net>
2018-11-09 15:43:28 -0500

    implement auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Link } from 'react-router-dom';
 import Todos from './Todos';
 import NewTodo from './NewTodo';
 import EditTodo from './EditTodo';
@@ -8,35 +8,71 @@ import Login from './Login';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.setUser = this.setUser.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+  }
+
   render() {
     return (
       <Router>
         <div>
           <nav className="navbar navbar-light navbar-expand bg-light mb-3">
             <Link to="/" className="navbar-brand mr-auto">Todos</Link>
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link to="/todos/new" className="nav-link">New Todo</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/logout" className="nav-link">Logout</Link>
-              </li>
-            </ul>
+              {this.state.user && (
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Link to="/todos/new" className="nav-link">New Todo</Link>
+                  </li>
+                  <li className="nav-item">
+                    <a href onClick={this.handleLogoutClick} className="nav-link">Logout</a>
+                  </li>
+                </ul>
+              )}
           </nav>
           <div className="container">
             <div className="alert alert-primary">
               <span>Oops, something went wrong logging in.</span>
             </div>
-            <Route exact path="/" component={Todos} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/login" component={Login} />
+            <Route exact path="/" render={props =>
+              this.state.user
+                ? <Redirect to="/todos" />
+                : <Redirect to="/login" /> } />
+            <Route path="/signup" render={props =>
+              this.state.user
+                ? <Redirect to="/todos" />
+                : <SignUp {...props} setUser={this.setUser}/> } />
+            <Route path="/login" render={props =>
+              this.state.user
+                ? <Redirect to="/todos" />
+                : <Login {...props} setUser={this.setUser}/> } />
+            <Route path="/todos" render={props =>
+              this.state.user
+                ? <Todos {...props} />
+                : <Redirect to="/login" /> } />
             <Route path="/todos/new" component={NewTodo} />
             <Route path="/todos/edit" component={EditTodo} />
-            <Route path="/logout" component={Login} />
           </div>
         </div>
       </Router>
     );
+  }
+
+  handleLogoutClick() {
+    fetch('http://localhost:5000/api/logout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      }
+    })
+    .then(response => response.json())
+    .then(data => this.setState({ user: undefined }))
+  }
+
+  setUser(user) {
+    this.setState({ user });
   }
 }
 
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,25 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 
-const SignUp = () =>
-  (
-    <div>
-      <h1 className="h3">Sign Up</h1>
-      <form>
-        <div className="form-group">
-          <label for="username">Username</label>
-          <input className="form-control" aria-describedby="username" required />
-        </div>
-        <div className="form-group">
-          <label for="password">Password</label>
-          <input type="password" className="form-control" aria-describedby="password" required />
-        </div>
-        <div className="form-group">
-          <Link to="/" className="btn btn-primary mr-2">Sign up</Link>
-          <Link to="/login" className="btn btn-link">Login</Link>
-        </div>
-      </form>
-    </div>
-  )
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '', password: '' };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="h3">Sign Up</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label for="username">Username</label>
+            <input className="form-control" onChange={this.handleUsernameChange} aria-describedby="username" required />
+          </div>
+          <div className="form-group">
+            <label for="password">Password</label>
+            <input type="password" className="form-control" onChange={this.handlePasswordChange} aria-describedby="password" required />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-primary mr-2">Sign up</button>
+            <Link to="/login" className="btn btn-link">Login</Link>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
+  handlePasswordChange({ target: { value } }) {
+    this.setState({ password: value });
+  } 
+
+  handleUsernameChange({ target: { value } }) {
+    this.setState({ username: value });
+  } 
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const newUser = {
+      username: this.state.username,
+      password: this.state.password
+    }
+    fetch('http://localhost:5000/api/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(newUser)
+    })
+    .then(response => response.json())
+    .then(this.props.setUser)
+  }
+}
 
 export default SignUp;
